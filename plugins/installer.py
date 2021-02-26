d382499134c02614195f160537d5ace5947e2acd
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-02-26 14:48:41 +0530

    Create installer.py

---
+++ plugins/installer.py
@@ -0,0 +1,37 @@
+import os
+from . import *
+
+
+@ultroid_cmd(pattern="instal")
+async def install(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = (
+                await event.client.download_media(  # pylint:disable=E0602
+                    await event.get_reply_message(),
+                    "plugins/",  # pylint:disable=E0602
+                )
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_plugins(shortname.replace(".py", ""))
+                await eor(
+                    event,
+                    "Plugin successfully installed\n {}".format(
+                        os.path.basename(downloaded_file_name)
+                    ),
+                )
+            else:
+                os.remove(downloaded_file_name)
+                await eor(
+                    event,
+                    "Error!\nPlugin cannot be installed!\n Or may have been pre-installed.",
+                )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await eor(event, str(e))
+            os.remove(downloaded_file_name)
+    await asyncio.sleep(DELETE_TIMEOUT)
+    await event.delete()
